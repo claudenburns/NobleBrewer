bc1a23af5657029aebe87d8d2182899c96997219
Backup log in <backuplogin@ubuntu.ubuntu-domain>
2013-12-23 15:03:26 -0500

    my django app no Procfile change url

--- infosite/views.py
+++ infosite/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'infosite/index.html')
 
-##from django.shortcuts import render_to_response
-##def index(request):
-##   return render_to_response('infosite/index.html')
+def about(request):
+    return render(request, 'infosite/about.html')
+
+
